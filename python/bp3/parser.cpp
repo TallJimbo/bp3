1ca372f95a97955e2bdaaa5517d78bcfeddcbe7d
Jim Bosch <jbosch@astro.princeton.edu>
2013-05-20 21:53:54 -0400

    Add libclang search to cmake, extend stub a bit in parser mod

--- python/bp3/parser.cpp
+++ python/bp3/parser.cpp
@@ -1,12 +1,38 @@
 #include "bp3/builtin.hpp"
 
+#include <vector>
+
+#include "clang-c/Index.h"
+
 namespace bp3 {
 
+static PyObject * run(PyObject * self, PyObject * args) {
+    Py_ssize_t nargs = PySequence_Size(args);
+    std::vector<std::string> str_args(nargs);
+    std::vector<char const *> cstr_args(nargs);
+    try {
+        for (Py_ssize_t i = 0; i < nargs; ++i) {
+            py_ptr item = py_ptr::steal(PySequence_GetItem(args, i));
+            str_args[i] = builtin::str(item);
+            cstr_args[i] = str_args[i].c_str();
+        }
+    } catch (builtin::Exception & err) {
+        return err.release();
+    }
+    CXIndex index = clang_createIndex(0, 0);
+    CXTranslationUnit tu = clang_parseTranslationUnit(index, 0, &cstr_args.front(), nargs, nullptr, 0,
+                                                      CXTranslationUnit_None);
+    
 
+    clang_disposeTranslationUnit(tu);
+    clang_disposeIndex(index);
+    Py_RETURN_NONE;
+}
 
 } // namspace bp3
 
 static PyMethodDef methods[] = {
+    {"run", (PyCFunction)&bp3::run, METH_VARARGS, nullptr},
     {nullptr, nullptr, 0, nullptr}
 };
 
---
+++ scratch/parser-practice.cpp
@@ -0,0 +1,10 @@
+class Example1 {
+public:
+
+    int method1(double x) const;
+
+    void method2();
+
+    int pub_value1;
+
+};
